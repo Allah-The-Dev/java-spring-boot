159b186102a22d196cc507138d101444ba8dc650
Neelesh Pandey <pandeyneelkumar@gmail.com>
2021-01-23 15:41:52 +0530

    after logout show index page

--- TechnicalBlogApplication/src/main/java/technicalblog/controller/UserController.java
+++ TechnicalBlogApplication/src/main/java/technicalblog/controller/UserController.java
@@ -1,13 +1,22 @@
 package technicalblog.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import technicalblog.model.Post;
 import technicalblog.model.User;
+import technicalblog.service.PostService;
 
 @Controller
 public class UserController {
+
+    @Autowired
+    private PostService postService;
 
     @RequestMapping("users/login")
     public String login() {
@@ -23,5 +32,14 @@ public class UserController {
     public String loginUser(User user) {
         return "redirect:/posts";
     }   
+
+    @RequestMapping(value = "users/logout", method=RequestMethod.POST)
+    public String logoutUser(Model model) {
+
+        List<Post> posts = postService.getAllPosts();
+        model.addAttribute("posts", posts);
+
+        return "index";
+    }
 
 }
