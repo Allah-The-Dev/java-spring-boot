a030e91b935ec3d9913dc15d30a5ce8ce40180ce
Neelesh Pandey <pandeyneelkumar@gmail.com>
2021-01-24 16:06:55 +0530

    basic home controller santiry check to test body contains proper heading

--- TechnicalBlogApplication/src/test/java/technicalblog/HomeControllerTest.java
+++ TechnicalBlogApplication/src/test/java/technicalblog/HomeControllerTest.java
@@ -5,11 +5,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 
 import technicalblog.controller.HomeController;
 
-@SpringBootTest()
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class HomeControllerTest {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Autowired
 	private HomeController controller;
@@ -21,6 +30,12 @@ public class HomeControllerTest {
 	@Test
 	public void controllerIsNotNull() throws Exception {
 		assertThat(controller).isNotNull();
+	}
+
+	@Test
+	public void homeContrllerReponseShouldConntainExpected() throws Exception {
+		String url = "http://localhost:" + port + "/";
+		assertThat(this.restTemplate.getForObject(url, String.class)).contains("<h1>All Technical Posts</h1>");
 	}
 
 }
