ced13f7e8d41ace99b0b12f601e5cca1f6d4c335
Neelesh Pandey <pandeyneelkumar@gmail.com>
2021-02-06 13:29:52 +0530

    user registration : controller, service and repository code

--- TechnicalBlogApplication/src/main/java/technicalblog/controller/UserController.java
+++ TechnicalBlogApplication/src/main/java/technicalblog/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import technicalblog.model.Post;
 import technicalblog.model.User;
+import technicalblog.model.UserProfile;
 import technicalblog.service.PostService;
 import technicalblog.service.UserService;
 
@@ -49,12 +50,18 @@ public class UserController {
 
     @RequestMapping("users/registration")
     public String registration(Model model) {
-        model.addAttribute("User", new User());
+        User user = new User();
+        UserProfile profile = new UserProfile();
+        user.setProfile(profile);
+
+        model.addAttribute("User", user);
+
         return "users/registration";
     }
     
     @RequestMapping(value = "users/registration", method=RequestMethod.POST)
     public String registerUser(User user) {
+        userService.registerUser(user);
         return "users/login";
     }   
 
--- TechnicalBlogApplication/src/main/java/technicalblog/service/UserService.java
+++ TechnicalBlogApplication/src/main/java/technicalblog/service/UserService.java
@@ -1,11 +1,20 @@
 package technicalblog.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import technicalblog.model.User;
+import technicalblog.repository.UserRepository;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public void registerUser(User newUser) {
+        repository.registerUser(newUser);
+    }
 
     public boolean login(User user) {
         if(user.getUsername().equals("validuser")) {
